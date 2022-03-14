dd8f379c46ab2d425c048ebd4cc6cd8b2e494fec
Felix Rochau <felixrochau@gmx.de>
2022-03-14 14:36:26 +0100

    add docstrings for preprocessing script

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -6,20 +6,27 @@ import os
 from sklearn.model_selection import train_test_split
 
 def load_drive_stats(filename, path) -> pd.DataFrame:
-    """_summary_
+    """Load drive stats file
 
     Args:
-        filename (str, optional): _description_. Defaults to "ST4000DM000_history".
+        filename (_type_): Name of the csv file
+        path (_type_): Path of the repo
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: Dataframe containing the drive stats
     """
     file = f"{path}/data/raw/{filename}.csv"
     df = pd.read_csv(file, parse_dates=["date"])
     return df
 
 def countdown(df) -> pd.DataFrame:
-    """create failure date column, calculate countdown
+    """Create column with failure date and calculate countdown
+
+    Args:
+        df (_type_): Drive stats file
+
+    Returns:
+        pd.DataFrame: Drive stats with countdown column
     """
     # Series of all the hdds the day they failed to obtain failure date
     failure = df[df.failure == 1]
@@ -33,7 +40,18 @@ def countdown(df) -> pd.DataFrame:
     df = df[df.countdown >= 0]
     return df
 
-def train_test_splitter(df, test_size, random_state, stratify=True):
+def train_test_splitter(df, test_size, random_state, stratify=True) -> pd.DataFrame:
+    """Train test split of the drive data
+
+    Args:
+        df (_type_): Drive stats
+        test_size (_type_): Size of the test set
+        random_state (_type_): Random state for comparability over different runs
+        stratify (bool, optional): Stratification. Defaults to True.
+
+    Returns:
+        pd.DataFrame: Split features and targets
+    """
     X = df.copy()
     y = X.pop("countdown")
     if stratify:
@@ -43,6 +61,15 @@ def train_test_splitter(df, test_size, random_state, stratify=True):
     return X_train, X_test, y_train, y_test
 
 def drop_missing_cols(df, threshold=0.8) -> pd.DataFrame:
+    """Drop columns with missing values. A threshold allows to tune which columns are dropped.
+
+    Args:
+        df (_type_): Drive stats data
+        threshold (float, optional): Percentage of missings in a column so that it is dropped. Defaults to 0.8.
+
+    Returns:
+        pd.DataFrame: Drive stats file with dropped columns
+    """
     cols_to_drop = df.columns[df.notna().sum() < (threshold * len(df))] # Columns that contain lot of NaNs
     #print("Number of columns to drop:", len(cols_to_drop))
     df = df.drop(cols_to_drop, axis=1) # Drop the cols
@@ -50,6 +77,14 @@ def drop_missing_cols(df, threshold=0.8) -> pd.DataFrame:
     return df
 
 def drop_constant_cols(df) -> pd.DataFrame:
+    """Drop columns with constant values since they play no role for modeling
+
+    Args:
+        df (_type_): Drive stats data
+
+    Returns:
+        pd.DataFrame: Drive stats file with dropped columns
+    """
     # check columns which only contain 0 values and drop them from the data frame
     cols_to_drop = df.describe().T.query('std == 0').reset_index()['index'].to_list()
     #print(cols_to_drop)
@@ -57,15 +92,45 @@ def drop_constant_cols(df) -> pd.DataFrame:
     return df
 
 def drop_missing_rows(df) -> pd.DataFrame:
+    """Drop rows with missing values (measurement errors, see EDA)
+
+    Args:
+        df (_type_): Drive stats
+
+    Returns:
+        pd.DataFrame: Drive stats data with removed rows
+    """
     df = df.dropna(how="any")
     return df
 
-def save_preprocessed_data(filename="ST4000DM000_history", path=os.getcwd()):
+def load_preprocess_data(filename="ST4000DM000_history_total", path=os.getcwd()) -> pd.DataFrame:
+    """Load and preprocess drive stats data
+
+    Args:
+        filename (str, optional): Name of the csv file. Defaults to "ST4000DM000_history".
+        path (_type_, optional): Path of the repo. Defaults to os.getcwd().
+
+    Returns:
+        pd.DataFrame: Dataframe with the drive stats data
+    """
     df = load_drive_stats(filename, path)
     df = countdown(df)
     df = drop_missing_cols(df)
     df = drop_missing_rows(df)
     df = drop_constant_cols(df)
+    return df
+
+def save_preprocessed_data(filename="ST4000DM000_history_total", path=os.getcwd()):
+    """Load and preprocess the drive stats data and store the result in a csv file
+
+    Args:
+        filename (str, optional): Name of the csv file. Defaults to "ST4000DM000_history".
+        path (_type_, optional): Path of the repo. Defaults to os.getcwd().
+
+    Returns:
+        pd.DataFrame: Dataframe with the drive stats data
+    """
+    df = load_preprocess_data(filename=filename, path=path)
     file = f"{path}/data/processed/{filename}_preprocessed.csv"
     folder = f"{path}/data/processed/"
     if not os.path.exists(folder):
@@ -73,4 +138,5 @@ def save_preprocessed_data(filename="ST4000DM000_history", path=os.getcwd()):
     df.to_csv(file, index=False)
     return df
 
-#save_preprocessed_data()
+if __name__ == "__main__":
+    _ = save_preprocessed_data()
